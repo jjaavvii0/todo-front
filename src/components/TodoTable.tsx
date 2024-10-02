029862ab2006dee2dd5edc23cbab20ec209d1eb0
J P <jjaavvii0@gmail.com>
2024-10-02 21:15:30 +0100

    include button to store new data

--- src/components/TodoTable.tsx
+++ src/components/TodoTable.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Alert, Button, Input, Space, Table } from "antd";
 import type { TableProps } from "antd";
 import { DeleteOutlined } from "@ant-design/icons";
@@ -11,15 +11,43 @@ interface DataType {
 }
 interface EditingState {
     row: number | null;
-    column: string; // TODO: Hacer un ENUM
+    column: string; // TODO: Do with ENUM
     data: Partial<DataType>;
 }
 export function TodoTable() {
+    const [data, setData] = useState<DataType[]>([
+        {
+            id: 1,
+            name: "Do the technical test",
+            description: "Area55",
+            status: false,
+        },
+        {
+            id: 2,
+            name: "Do the laundry",
+            description: "All the T-shirts",
+            status: false,
+        },
+        {
+            id: 3,
+            name: "Clean the living room",
+            status: false,
+        },
+    ]);
     const [editing, setEditing] = useState<EditingState>({
         row: null,
         column: "",
         data: {},
     });
+    const [dataToStore, setDataToStore] = useState(false);
+    const dataReference = useRef<DataType[]>(data);
+
+    const isEqual = (obj1: object, obj2: object): boolean => {
+        return JSON.stringify(obj1) === JSON.stringify(obj2);
+    };
+    useEffect(() => {
+        setDataToStore(isEqual(dataReference.current, data));
+    }, [data]);
 
     const columns: TableProps<DataType>["columns"] = [
         {
@@ -31,15 +59,12 @@ export function TodoTable() {
             title: "Name",
             dataIndex: "name",
             key: "name",
-            //RENDER:
-            //Primer parámetro representa el valor de la celda para esa columna particular
-            //Segundo parámetro es el objeto que representa toda la fila
             render: (value, record): any =>
                 editing.row !== null &&
                 record.id === editing.row &&
                 editing.column === "name" ? (
                     <Input
-                    autoFocus
+                        autoFocus
                         onChange={(e) =>
                             setEditing({
                                 ...editing,
@@ -57,7 +82,11 @@ export function TodoTable() {
                         onBlur={() => handleSaveNewData(record.id)}
                     ></Input>
                 ) : (
-                    <span onDoubleClick={() => handleEditingCell(record.id, "name")}>
+                    <span
+                        onDoubleClick={() =>
+                            handleEditingCell(record.id, "name")
+                        }
+                    >
                         {value}
                     </span>
                 ),
@@ -72,7 +101,7 @@ export function TodoTable() {
                 record.id === editing.row &&
                 editing.column === "description" ? (
                     <Input
-                    autoFocus
+                        autoFocus
                         onChange={(e) =>
                             setEditing({
                                 ...editing,
@@ -104,22 +133,14 @@ export function TodoTable() {
             title: "Status",
             key: "status",
             dataIndex: "status",
-            render: (value, record) =>
-                value ? (
-                    <Alert
-                        className="custom-alert"
-                        message="DONE"
-                        type="success"
-                        onClick={() => toggleStatus(record.id)}
-                    />
-                ) : (
-                    <Alert
-                        className="custom-alert"
-                        message="PENDING"
-                        type="error"
-                        onClick={() => toggleStatus(record.id)}
-                    />
-                ),
+            render: (value, record) => (
+                <Alert
+                    className="custom-alert"
+                    message={value ? "DONE" : "PENDING"}
+                    type={value ? "success" : "error"}
+                    onClick={() => toggleStatus(record.id)}
+                />
+            ),
         },
         {
             title: "Actions",
@@ -136,26 +157,6 @@ export function TodoTable() {
         },
     ];
 
-    const [data, setData] = useState<DataType[]>([
-        {
-            id: 1,
-            name: "Do the technical test",
-            description: "Area55",
-            status: false,
-        },
-        {
-            id: 2,
-            name: "Do the laundry",
-            description: "All the T-shirts",
-            status: false,
-        },
-        {
-            id: 3,
-            name: "Clean the living room",
-            status: false,
-        },
-    ]);
-
     const toggleStatus = (id: number) => {
         setData((prevData) =>
             prevData.map((item) =>
@@ -165,7 +166,7 @@ export function TodoTable() {
     };
 
     const handleEditingCell = (id: number, column: string) => {
-        setEditing({...editing, column:column, row:id});
+        setEditing({ ...editing, column: column, row: id });
     };
 
     const handleSaveNewData = (id: number) => {
@@ -181,10 +182,25 @@ export function TodoTable() {
         });
     };
 
+    const handleDataToStore = () => {
+        setDataToStore(!dataToStore)
+        console.log("Sending to server"); //TODO: REQUEST
+        dataReference.current = data
+    }
+
     return (
-        <Table<DataType>
-            columns={columns}
-            dataSource={data}
-        />
+        <>
+            <Table<DataType>
+                columns={columns}
+                dataSource={data}
+            />
+            <Button
+                type="primary"
+                disabled={dataToStore}
+                onClick={handleDataToStore}
+            >
+                Store new data
+            </Button>
+        </>
     );
 }
